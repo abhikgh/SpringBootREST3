c8c4e6561aff79f6e9b84f450951f6af515db003
Abhik <abhikgh.work@gmail.com>
2024-07-22 19:53:23 +0530

    updates

--- src/main/java/com/example/SpringBootREST3/config/NavController.java
+++ src/main/java/com/example/SpringBootREST3/config/NavController.java
@@ -1,9 +1,12 @@
 package com.example.SpringBootREST3.config;
 
+import com.example.SpringBootREST3.model.Person;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,11 @@ public class NavController {
     public ResponseEntity<String> hello2() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body("Hello World 2");
+    }
+
+    @PostMapping(value = "/helloPost", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> helloPost(@RequestBody Person person) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(person.getFirstName().concat("----").concat(person.getLastName()));
     }
 }
---
+++ src/main/java/com/example/SpringBootREST3/model/Person.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootREST3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+    private String firstName;
+    private String lastName;
+}
