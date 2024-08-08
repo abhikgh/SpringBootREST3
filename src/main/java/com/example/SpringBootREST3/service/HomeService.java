9fda9b17f58946a948292968c0ef0f45e825c4cf
Abhik <abhikgh.work@gmail.com>
2024-08-08 21:56:05 +0530

    updates

--- src/main/java/com/example/SpringBootREST3/service/HomeService.java
+++ src/main/java/com/example/SpringBootREST3/service/HomeService.java
@@ -2,6 +2,7 @@ package com.example.SpringBootREST3.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -10,6 +11,19 @@ import java.util.concurrent.TimeUnit;
 public class HomeService {
 
     private static final Logger logger  = LoggerFactory.getLogger(HomeService.class);
+
+    @Value("${value123}")
+    private String value123;
+
+    public int validateTestValue(){
+        if(value123.equalsIgnoreCase("100"))
+            return 100;
+        else if(value123.equalsIgnoreCase("200"))
+            return 200;
+        else
+            return 300;
+
+    }
 
     public String getResponse(){
         //Adding sleep
