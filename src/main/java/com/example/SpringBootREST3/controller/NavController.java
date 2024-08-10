9a7136f1d4b0b706fbfcb16181260ed50ceb06aa
Abhik <abhikgh.work@gmail.com>
2024-08-10 13:04:17 +0530

    updates

--- src/main/java/com/example/SpringBootREST3/controller/NavController.java
+++ src/main/java/com/example/SpringBootREST3/controller/NavController.java
@@ -2,6 +2,8 @@ package com.example.SpringBootREST3.controller;
 
 import com.example.SpringBootREST3.model.Person;
 import com.example.SpringBootREST3.service.HomeService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,8 +21,18 @@ public class NavController {
     @Autowired
     private HomeService homeService;
 
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> hello() {
+
+        //Counter metric
+        Counter counter = Counter.builder("hello")
+                .description("gets number of requests to /hello endpoint")
+                .register(meterRegistry);
+        counter.increment();
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body("Hello World");
     }
