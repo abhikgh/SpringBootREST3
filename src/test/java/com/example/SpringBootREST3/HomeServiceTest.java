439d40e082cd90f0f3f231ffcb60cb7bfc9f4d65
Abhik <abhikgh.work@gmail.com>
2025-03-21 16:32:24 +0530

    updates

---
+++ src/test/java/com/example/SpringBootREST3/HomeServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.SpringBootREST3;
+
+import com.example.SpringBootREST3.service.HomeService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith({SpringExtension.class})
+class HomeServiceTest {
+
+    @InjectMocks
+    @Spy
+    private HomeService homeService;
+
+    @BeforeEach
+    public void init() {
+        //set private field
+        ReflectionTestUtils.setField(homeService, "value123", "123");
+    }
+
+    @Test
+    void testValidateTestValue() {
+        Assertions.assertEquals(300, homeService.validateTestValue());
+        ReflectionTestUtils.setField(homeService, "value123", "200");
+        Assertions.assertEquals(200, homeService.validateTestValue());
+        ReflectionTestUtils.setField(homeService, "value123", "hdhdhdhd");
+        Assertions.assertEquals(300, homeService.validateTestValue());
+    }
+
+
+}
+
--- src/test/java/com/example/SpringBootREST3/HomeServiceTest2.java
+++ src/test/java/com/example/SpringBootREST3/HomeServiceTest2.java
@@ -1,3 +1,5 @@
+package com.example.SpringBootREST3;
+
 import com.example.SpringBootREST3.service.HomeService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -5,19 +7,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-@ExtendWith({SpringExtension.class})
-class HomeServiceTest {
+@ExtendWith(MockitoExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class HomeServiceTest2 {
 
-    @InjectMocks
-    @Spy
     private HomeService homeService;
 
     @BeforeEach
     public void init() {
         //set private field
+        homeService = new HomeService();
         ReflectionTestUtils.setField(homeService, "value123", "123");
     }
 
