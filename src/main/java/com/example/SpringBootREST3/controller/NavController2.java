e80461f10e6839568cd19eeb5ad361224a2ca3af
Abhik <abhikgh.work@gmail.com>
2025-04-07 21:06:55 +0530

    updates

--- src/main/java/com/example/SpringBootREST3/controller/NavController2.java
+++ src/main/java/com/example/SpringBootREST3/controller/NavController2.java
@@ -3,8 +3,10 @@ package com.example.SpringBootREST3.controller;
 import com.example.SpringBootREST3.exception.ErrorProperties;
 import com.example.SpringBootREST3.model.OrderRequestForm;
 import com.example.SpringBootREST3.model.OrderResponse;
+import com.example.SpringBootREST3.model.PushMessageBody;
 import com.example.SpringBootREST3.service.HomeService;
 import com.example.SpringBootREST3.util.OrderServiceUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 @RestController
@@ -72,7 +76,37 @@ public class NavController2 {
                 .status(HttpStatus.OK)
                 .header("test", "test")
                 .body(orderResponse);
+    }
 
+    //http://localhost:9100/v3/rest/getOrder4/Feb/red?parmRequestSource=web&parmAudienceType=external
+    /*
+    {
+        "data": "ewogICAgIm9yZGVySWQiOiAxMDAsCiAgICAiaXRlbSI6ICJpUGhvbmUiLAogICAgIml0ZW1UeXBlIjogIlBob25lIiwKICAgICJpbnZvaWNlVHlwZSI6IDIKfQ=="
+    }
+     */
+
+    @PostMapping(value = "/getOrder4/{month}/{colour}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<OrderResponse> getOrder4(
+            @PathVariable(value = "month") String month,
+            @PathVariable(value = "colour") String colour,
+            @RequestParam(value = "parmRequestSource") String parmRequestSource,
+            @RequestParam(required = false, value = "parmAudienceType") String parmAudienceType,
+            @RequestHeader(value = "Actor") String actor,
+            @RequestBody PushMessageBody pushMessageBody) throws IOException {
+        OrderRequestForm orderRequestForm = new ObjectMapper().readValue(Base64.getDecoder().decode(pushMessageBody.getData()), OrderRequestForm.class);
+        OrderResponse orderResponse = new OrderResponse();
+        String item = orderRequestForm.getItem();
+
+        if (!orderServiceUtil.allowedItems.contains(item)) {
+            errorProperties.throwSPEException(ErrorProperties.ErrorCode.INVALID_ITEM, List.of(item));
+        } else {
+            orderResponse = homeService.getOrderResponse(orderResponse, orderRequestForm, parmAudienceType, parmRequestSource, month, actor);
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("test", "test")
+                .body(orderResponse);
     }
 
 }
---
+++ src/main/java/com/example/SpringBootREST3/model/PushMessageBody.java
@@ -0,0 +1,11 @@
+package com.example.SpringBootREST3.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PushMessageBody {
+    private String data;
+}
